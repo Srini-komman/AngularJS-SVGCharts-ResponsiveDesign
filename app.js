814121cc8f830c8ba9b5577e46ee9e9f7d9d194f
Srini <Srini.komman@gmail.com>
2016-02-18 20:52:51 +1100

    angular routing mechanism has been implemented to the app

--- app.js
+++ app.js
@@ -1,2 +1,14 @@
 ﻿angular
-    .module('countryapp', ['ngRoute']);
+    .module('countryapp', ['ngRoute'])
+    .config(config);
+
+function config($routeProvider) {
+    $routeProvider
+        .when('/', {
+            templateUrl: 'views/countries-population.html',
+            controller: 'CountryController'
+        })
+        .otherwise({
+            redirectTo: '/'
+        })
+}
